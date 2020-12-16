afcd4798eab13c5f10582ab3d5b8eba6c063921f
pavelgarbuz <pavel.garbuz@jetbi.com>
2020-12-16 16:40:54 +0200

    init project

---
+++ codecept.conf.js
@@ -0,0 +1,58 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+		Puppeteer: {
+			restart: true,
+			pressKeyDelay: 4,
+			waitForNavigation: ['networkidle2'],
+			waitForAction: 200,
+			url: 'http://localhost',
+			show: true,
+			windowSize: '1920x850',
+			chrome: {
+				'args': [
+					'--user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"',
+					'--disable-setui-sandbox',
+					'--ignore-certificate-errors',
+					'--ignore-certificate-errors-spki-list',
+					'--allow-insecure-localhost',
+					'--allow-running-insecure-content',
+					'--no-sandbox',
+					'--start-maximized',
+					'--disable-web-security',
+					'--enable-potentially-annoying-security-features',
+					'--enable-site-per-process',
+				],
+				ignoreHTTPSErrors: true,
+			}
+		}
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'codeceptjs_puppeteer_DDT',
+  plugins: {
+		pauseOnFail: {},
+		retryFailedStep: {
+			enabled: true,
+			retries: 25,
+			defaultIgnoredSteps: ['wait*', 'execute*', 'run*', 'have*', 'send*']
+		},
+		tryTo: {
+			enabled: true
+		},
+		screenshotOnFail: {
+			enabled: true
+		},
+		allure: {
+			enabled: true
+		}
+  }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+npm init
+npm i codeceptjs puppeteer@1.19 allure-commandline --save-dev
+npx codeceptjs init
+
+git init
+=== add .gitignore file ===
+=== create repository ===
+git add
